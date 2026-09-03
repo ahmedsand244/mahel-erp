a6d6ca6687781123a6bcfeeb0bf1d946be25e56a
ahmedsand244 <ahmedsand244@gmail.com>
2026-09-03 20:34:53 +0300

    Fixed offline CSS/JS styles: Switched CDN Tailwind/Fonts to local offline vendor assets across all tenant templates

--- core_project/urls.py
+++ core_project/urls.py
@@ -58,6 +58,10 @@ urlpatterns = [
     path('backup/',      include('dashboard.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': os.path.join(settings.BASE_DIR, 'static')}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
